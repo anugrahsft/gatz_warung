6a17d66824c3caf7fa079c4f4def69f0edadef3d
anugrahsft <muhanugrah@outlook.com>
2026-01-05 19:43:33 +0800

    "hapus ui streamlit di mobile"

--- warung_gatz.py
+++ warung_gatz.py
@@ -3,6 +3,16 @@ import pandas as pd
 from database_helper import get_connection
 
 st.set_page_config(page_title="Gatz Warung Digital", layout="wide")
+st.markdown(
+    """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 # --- FUNGSI DATABASE ---
 def simpan_barang(nama, modal, jual, stok):
